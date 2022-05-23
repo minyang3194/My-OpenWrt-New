37e4cada42e103d21070a44f4e72fdc212fa4002
HiJwm <104122387+HiJwm@users.noreply.github.com>
2022-05-23 17:16:27 +0800

    Update op2.sh

--- diy/official/op2.sh
+++ diy/official/op2.sh
@@ -59,7 +59,8 @@ rm -rf feeds/packages/utils/coremark
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/coremark feeds/packages/utils/coremark 
 
 ##添加adbybyplus插件
-svn export https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-adbyby-plus ./package/luci-app-adbyby-plus
+svn export https://github.com/Lienol/openwrt-package/branches/other/lean/luci-app-adbyby-plus ./package/luci-app-adbyby-plus #无adbyby包
+svn export https://github.com/Lienol/openwrt-package/branches/other/lean ./package/other #全
 
 ##FQ全部调到VPN菜单
 sed -i 's/services/vpn/g' package/feeds/xiangfeidexiaohuo/luci-app-ssr-plus/luasrc/controller/*.lua
