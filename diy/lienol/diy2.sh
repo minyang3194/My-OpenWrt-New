5b1a132aeccb92f21f621252161c9ae1a7b5572a
HiJwm <104122387+HiJwm@users.noreply.github.com>
2022-06-03 14:56:12 +0800

    Update diy2.sh

--- diy/lienol/diy2.sh
+++ diy/lienol/diy2.sh
@@ -19,6 +19,9 @@ svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds
 ##配置IP
 sed -i 's/192.168.1.1/192.168.100.101/g' package/base-files/files/bin/config_generate
 
+##删除版本中Lienol
+sed -i '/Lienol/d' ./feeds/luci/luci.mk
+
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
