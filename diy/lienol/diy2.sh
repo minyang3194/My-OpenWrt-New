e6be69345c865ef6ac427bb7f8c1cee2715eb52e
HiJwm <104122387+HiJwm@users.noreply.github.com>
2022-06-09 21:42:26 +0800

    Update diy2.sh

--- diy/lienol/diy2.sh
+++ diy/lienol/diy2.sh
@@ -20,7 +20,7 @@ svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds
 sed -i 's/192.168.1.1/192.168.100.101/g' package/base-files/files/bin/config_generate
 
 ##删除版本中Lienol
-sed -i 's/Lienol/branch @ HiJwm-$(date +%Y%m%d)/' ./feeds/luci/luci.mk
+sed -i 's/Lienol/branch @ HiJwm-${{ env.FILE_DATE }}/' ./feeds/luci/luci.mk
 ##更改luci
 #wget -O ./feeds/luci/luci.mk https://raw.githubusercontent.com/openwrt/luci/openwrt-22.03/luci.mk
 
