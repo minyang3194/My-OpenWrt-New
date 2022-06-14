18895939563ec5a6e1ce3b62e952e7d2f7050739
HiJwm <104122387+HiJwm@users.noreply.github.com>
2022-06-14 12:28:38 +0800

    Update lean2.sh

--- diy/lean/lean2.sh
+++ diy/lean/lean2.sh
@@ -38,9 +38,9 @@ sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt-$(date +%Y%m%d)
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
 
 ##修改autocore
-#rm -rf ./package/lean/autocore
-#rm -rf ./feeds/xiangfeidexiaohuo/patch/autocore
-#svn export https://github.com/HiJwm/MySettings/trunk/lean/myautocore feeds/xiangfeidexiaohuo/patch/autocore
+rm -rf ./package/lean/autocore
+rm -rf ./feeds/xiangfeidexiaohuo/patch/autocore
+svn export https://github.com/HiJwm/MySettings/trunk/lean/myautocore feeds/xiangfeidexiaohuo/patch/autocore
 ##修改概述内容 上述已经修改，本条无意义
 #wget -O ./feeds/xiangfeidexiaohuo/patch/autocore/files/x86/index.htm https://raw.githubusercontent.com/HiJwm/MySettings/main/lean/index.htm
 
