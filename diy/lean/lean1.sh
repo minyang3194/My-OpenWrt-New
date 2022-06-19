c203defb063b84819b525e5fccef36a22d45bad0
HiJwm <104122387+HiJwm@users.noreply.github.com>
2022-06-19 18:36:24 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -8,7 +8,9 @@ rm -rf ./package/lean/k3screenctrl
 rm -rf ./package/lean/autocore
                
 
-sed -i "1isrc-git mypackages https://github.com/HiJwm/OP-Packages" feeds.conf.default
+#sed -i "1isrc-git mypackages https://github.com/HiJwm/OP-Packages" feeds.conf.default
+sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" feeds.conf.default
+
 sed -i "2isrc-git passwall https://github.com/xiaorouji/openwrt-passwall" feeds.conf.default
 sed -i "3isrc-git helloworld https://github.com/fw876/helloworld" feeds.conf.default
 sed -i "4isrc-git ipkg https://github.com/xiangfeidexiaohuo/op-ipkg\n" feeds.conf.default
