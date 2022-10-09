ee6761b7ed806ae622ec67088c79b76bd6a795e2
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2022-10-09 19:48:07 +0800

    Update op2.sh

--- diy/official/op2.sh
+++ diy/official/op2.sh
@@ -23,7 +23,7 @@ svn export https://github.com/coolsnowwolf/packages/trunk/utils/runc feeds/packa
 svn export https://github.com/coolsnowwolf/packages/trunk/utils/libnetwork feeds/packages/utils/libnetwork
 
 ##配置IP
-sed -i 's/192.168.1.1/192.168.100.101/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.99/g' package/base-files/files/bin/config_generate
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
